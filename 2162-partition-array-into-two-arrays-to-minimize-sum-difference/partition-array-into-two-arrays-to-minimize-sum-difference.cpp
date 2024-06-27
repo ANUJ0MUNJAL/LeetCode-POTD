a16aa37c4d19b7442aa3bf5ea3920f020cfd89bc
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2024-06-27 12:47:27 +0530

    Time: 1009 ms (6.93%) | Memory: 163.6 MB (7.33%) - LeetSync

---
+++ 2162-partition-array-into-two-arrays-to-minimize-sum-difference/partition-array-into-two-arrays-to-minimize-sum-difference.cpp
@@ -0,0 +1,85 @@
+class Solution {
+public: 
+    int anuj=0;
+   
+    int minimumDifference(vector<int>& nums) {
+        
+        for(auto &j : nums){
+             anuj+=j;
+        }
+        vector<set<int>> arr(nums.size()/2+1);
+        int value = (1 <<nums.size()/2);
+        for(int mask = 0 ; mask < value  ; mask++){
+                
+
+              int val= mask;
+              int index =0;
+              int sum=0;
+              
+              while(val != 0){
+                   if(val & 1){
+                        sum+=nums[index];
+                   }
+                   index++;
+                   val=val/2;
+              }
+              
+              int count = __builtin_popcount(mask);
+
+              arr[count].insert(sum);
+
+        }
+        cout<<"anul lo"<<anuj<<endl;
+        int cnt=0;
+        // for(auto &i:arr) 
+        // {
+        //     cout<<cnt<<"--->";
+        //     cnt++;
+        //     for(auto &j:i) 
+        //     {
+                
+        //         cout<<j<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+        int mini=INT_MAX;
+
+      
+        for(int mask = 0 ; mask < (1 << nums.size()/2) ; mask++){
+                
+
+              int val= mask;
+              int index =nums.size()/2;
+              int sum=0;
+              while(val != 0){
+                   if(val & 1){
+                        sum+=nums[index];
+                   }
+                   index++;
+                   val=val/2;
+              }
+            //   cout<<sum<<" ";
+              
+
+              int count = __builtin_popcount(mask);
+              
+              int total = anuj/2 - sum;
+
+              auto itr = arr[nums.size()/2 - count].upper_bound(total);
+              if(itr != arr[nums.size()/2 - count].end()){
+                  int val=*itr;
+                  mini=min(mini,abs(anuj - 2*(val+sum)));
+              }
+
+              if(itr != arr[nums.size()/2 - count].begin()){
+                        itr --;
+                   int val=*itr;
+                   mini=min(mini,abs(anuj - 2*(val+sum)));
+              }
+           
+        }
+        return mini;
+
+        
+    } 
+};
