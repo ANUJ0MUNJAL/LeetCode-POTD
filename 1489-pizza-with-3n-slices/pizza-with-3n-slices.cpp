cfba13b561e82a21967bf10163c82f2720188aa6
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2024-07-04 10:24:59 +0530

    Time: 20 ms (16.05%) | Memory: 16.6 MB (56.17%) - LeetSync

--- 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
+++ 1489-pizza-with-3n-slices/pizza-with-3n-slices.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    long long dp1[1001][501];
-     long long dp2[1001][501];
-    int solve(vector<int>&slices,int index,int count,int end){
-         if(count == 0 || index > end)return 0;
-         if(dp1[index][count] != -1) return dp1[index][count];
-         int include=0;
-         include=slices[index]+solve(slices,index+2,count-1,end);
-         int exclude=solve(slices,index+1,count,end);
-         return dp1[index][count]=max(include,exclude);
-    }
-     int solve1(vector<int>&slices,int index,int count,int end){
-         if(count == 0 || index > end)return 0;
-         if(dp2[index][count] != -1)return dp2[index][count];
-         int include=0;
-         include=slices[index]+solve1(slices,index+2,count-1,end);
-         int exclude=solve1(slices,index+1,count,end);
-         return dp2[index][count]=max(include,exclude);
+    int solve(int index,int endindex,vector<int>&slices,int n,vector<vector<int>>&dp)
+    {
+        if(n==0||index>endindex)
+        {
+            return 0;
+        }
+        if(dp[index][n]!=-1)
+        {
+            return dp[index][n];
+        }
+        int take=slices[index]+solve(index+2,endindex,slices,n-1,dp);
+        int notake=solve(index+1,endindex,slices,n,dp);
+        return dp[index][n]=max(take,notake);
     }
     int maxSizeSlices(vector<int>& slices) {
-        memset(dp1,-1,sizeof(dp1));
-        memset(dp2,-1,sizeof(dp2));
-
-        return max(solve(slices,0,slices.size()/3,slices.size()-2),solve1(slices,1,slices.size()/3,slices.size()-1));
+        int k=slices.size();
+        vector<vector<int>>dp1(k,vector<int>(k/3+1,-1));
+        vector<vector<int>>dp2(k,vector<int>(k/3+1,-1));
+        int case1=solve(0,k-2,slices,k/3,dp1);
+        int case2=solve(1,k-1,slices,k/3,dp2);
+        return max(case1,case2);
     }
 };
