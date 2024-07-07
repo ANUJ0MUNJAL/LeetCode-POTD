ea9d887d27a873386dd8974072254d193d546840
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2024-07-07 09:10:30 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (30.43%) - LeetSync

---
+++ 1642-water-bottles/water-bottles.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int khali=0,ans=0;
+         while(numBottles){
+             ans+=numBottles;
+             khali+=numBottles%numExchange;
+             numBottles/=numExchange;
+             
+             numBottles+=khali/numExchange;
+             cout<<khali<<"\n";
+             khali%=numExchange;
+              
+         }
+         return ans;
+    }
+};
