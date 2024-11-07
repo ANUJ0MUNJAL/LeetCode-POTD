42e643b8b6a3bdda69069f283d64dd920484d0c3
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2024-11-07 12:19:34 +0530

    Time: 40 ms (26.12%) | Memory: 60.2 MB (74.40%) - LeetSync

---
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        int maxi=0;
+        for(int i=0;i<31;i++)
+        {
+              int mask = 1<<i;
+              int count=0;
+              for(int j=0;j<candidates.size();j++)
+              {
+                  if(mask & candidates[j])count++;
+              }
+              maxi=max(maxi,count);
+        }
+        return maxi;
+    }
+};
