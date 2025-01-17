7312b9b200e4392447e2fa1ca08c32918f1efba3
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2025-01-17 12:22:43 +0530

    Time: 1 ms (70.56%) | Memory: 264 MB (48.13%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     bool doesValidArrayExist(vector<int>& derived) {
-        int sum = accumulate(derived.begin(),derived.end(),0);
-        if(sum & 1)return false;
-        return true;
+        int val = 0;
+        for(auto j: derived)val ^= j;
+        return !val;
     }
 };
