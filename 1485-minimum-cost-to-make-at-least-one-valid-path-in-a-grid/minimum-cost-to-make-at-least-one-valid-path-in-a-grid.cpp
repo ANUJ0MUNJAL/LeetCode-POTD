a3ed47cc5222fe626c56563657430c1808900711
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2025-01-18 11:43:22 +0530

    Time: 53 ms (36.01%) | Memory: 19.7 MB (64.28%) - LeetSync

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        // Priority queue to store {cost, x, y}
+        priority_queue<array<int, 3>, vector<array<int, 3>>, greater<array<int, 3>>> pq;
+        pq.push({0, 0, 0});
+        
+        vector<vector<int>> visited(rows, vector<int>(cols, 0));
+        
+        // Direction arrays for [1 -> right, 2 -> left, 3 -> down, 4 -> up]
+        vector<int> dx = {0, 0, 1, -1};
+        vector<int> dy = {1, -1, 0, 0};
+        
+        while (!pq.empty()) {
+            auto el = pq.top();
+            pq.pop();
+            
+            int cost = el[0];
+            int x = el[1];
+            int y = el[2];
+            
+            // Mark as visited
+            if (visited[x][y]) continue;
+            visited[x][y] = 1;
+            
+            if (x == rows - 1 && y == cols - 1) {
+                return cost;
+            }
+           
+            for (int dir = 1; dir <= 4; ++dir) {
+                int newx = x + dx[dir - 1];
+                int newy = y + dy[dir - 1];
+              
+                if (newx >= 0 && newx < rows && newy >= 0 && newy < cols && !visited[newx][newy]) {
+                  
+                    if (dir == grid[x][y]) {
+                        pq.push({cost, newx, newy}); 
+                    } else {
+                        pq.push({cost + 1, newx, newy}); 
+                    }
+                }
+            }
+        }
+        
+        return -1; 
+    }
+};
