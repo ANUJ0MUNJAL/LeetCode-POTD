d676342f9a0788719c83a96b315eee7d6d2786ff
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2025-03-21 22:07:15 +0530

    Time: 1477 ms (5.05%) | Memory: 188.6 MB (5.25%) - LeetSync

---
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
+          map<string,vector<string>> mp;
+          map<string,int> indegree;
+          set<string> st(supplies.begin(),supplies.end());
+          set<string> ans(recipes.begin(),recipes.end());
+    
+        
+          for(int i=0;i<ingredients.size();i++)
+          {
+              int count = 0;
+              for(auto j: ingredients[i])
+              {
+                  
+                      mp[j].push_back(recipes[i]);
+                      indegree[recipes[i]]++;
+                   
+              }
+       
+            
+          }
+
+          for(auto j: mp)
+          {
+              cout<<j.first<<"-->";
+              for(auto i: j.second)
+              {
+                cout<<i<<" ";
+              }
+              cout<<"\n";
+          }
+          vector<string> answer;
+          queue<string> q;
+          for(auto j: supplies)q.push(j);
+
+
+          while(!q.empty())
+          {
+              auto el = q.front();
+              q.pop();
+
+              
+
+
+              for(auto &neigh: mp[el])
+              {
+                   indegree[neigh]--;
+                   if(indegree[neigh] == 0)
+                   {
+                      
+                      q.push(neigh);
+                      if(ans.find(neigh)!=ans.end())
+                      {
+                           answer.push_back(neigh);
+                      }
+                   }
+              }
+          }
+
+          return answer;
+
+          
+
+        
+    }
+};
