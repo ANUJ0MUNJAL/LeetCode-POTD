62bd479897c96eb50e51b668c51c7373b0a1f83f
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2025-04-13 11:02:34 +0530

    Time: 0 ms (100.00%) | Memory: 198.8 MB (100.00%) - LeetSync

---
+++ 3824-number-of-unique-xor-triplets-i/number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        if(nums.size() <= 2)return nums.size();
+
+        int ans = 1;
+        while(ans <= nums.size()){
+            ans = ans << 1;
+        }
+        return ans;
+    }
+};
