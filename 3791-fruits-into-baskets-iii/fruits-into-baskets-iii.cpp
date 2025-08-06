49bbbf987441a1a2f79e68fb76e9231764c3ece8
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2025-08-06 22:52:58 +0530

    Time: 96 ms (50.42%) | Memory: 211 MB (6.13%) - LeetSync

--- 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/fruits-into-baskets-iii.cpp
@@ -1,62 +1,80 @@
+
 class Solution {
-    int n;
-    vector<int> seg;
-    void Update(int p) { 
-        seg[p] = max(seg[p << 1], seg[p << 1 | 1]); 
-    }
-
-    void Build(int p, int l, int r, vector<int>& baskets) {
-        if (l == r) {
-            seg[p] = baskets[l];
-            return;
-        }
-        int mid = (l + r) >> 1;
-        Build(p << 1, l, mid, baskets);
-        Build(p << 1 | 1, mid + 1, r, baskets);
-        Update(p);
-    }
-    void Assign(int x, int v, int p, int l, int r) {
-        if (x < l || x > r) {
-            return;
-        }
-        if (l == r) {
-            seg[p] = v;
-            return;
-        }
-        int mid = (l + r) >> 1;
-        Assign(x, v, p << 1, l, mid);
-        Assign(x, v, p << 1 | 1, mid + 1, r);
-        Update(p);
-    }
-    int FirstLarger(int v, int p, int l, int r) {
-        if (seg[p] < v) {
-            return r + 1;
-        }
-        if (l == r) {
-            return r;
-        }
-        int mid = (l + r) >> 1;
-        int lf = FirstLarger(v, p << 1, l, mid);
-        if (lf <= mid) {
-            return lf;
-        }
-        return FirstLarger(v, p << 1 | 1, mid + 1, r);
-    }
-
+    
 public:
+
+    int N;
+vector<int> v, Tree;
+void buildTree(int tidx, int lo, int hi){
+
+    if(lo==hi){
+        Tree[tidx] = v[hi];
+        return;
+    }
+
+    int mid = (lo+hi)/2;
+
+    buildTree(2*tidx+1, lo, mid);
+    buildTree(2*tidx+2, mid+1, hi);
+
+    Tree[tidx] = max(Tree[2*tidx+1] ,Tree[2*tidx+2]);
+
+}
+
+void updateTree(int tidx, int lo, int hi, int idx, int val){
+
+    if(lo==hi){
+        Tree[tidx] = -1;
+        return;
+    }
+
+    int mid = (lo+hi)/2;
+
+    if(idx<=mid)
+        updateTree(2*tidx+1, lo, mid, idx, val);
+    else
+        updateTree(2*tidx+2, mid+1, hi, idx, val);
+
+    Tree[tidx] = max(Tree[2*tidx+1],Tree[2*tidx+2]);
+
+}
+
+  int find(int value,int low,int high,int index)
+  {
+       if(low == high)
+       {
+         
+          int val = Tree[index];
+          if(val >= value)
+          updateTree(1,0,N-1,low,-1);
+          
+          return val;
+       }
+         int mid = (low+high)/2;
+   
+       if(Tree[2*index+1] >= value)
+       {
+          return find(value,low,mid,2*index+1);
+       }else{
+           return find(value,mid+1,high,2*index+2);
+       }
+  }
+
+
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        n = fruits.size();
-        seg.assign(4 * n + 1, 0);
-        Build(1, 0, n - 1, baskets);
-        int res = 0;
-        for (const auto& x : fruits) {
-            int pos = FirstLarger(x, 1, 0, n - 1);
-            if (pos == n) {
-                res++;
-            } else {
-                Assign(pos, 0, 1, 0, n - 1);
-            }
+        N = baskets.size();
+        v = baskets;
+        Tree.resize(10*N);
+        buildTree(1,0,baskets.size()-1);
+        int count = 0;
+        for(int i=0;i<fruits.size();i++)
+        {
+            // for(auto j: Tree)cout<<j<<" ";
+            int value = find(fruits[i],0,baskets.size()-1,1);
+            // cout<<value<<endl;
+            if(value < fruits[i])count++;
         }
-        return res;
+        return count;
+
     }
 };
