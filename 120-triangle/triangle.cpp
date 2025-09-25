597d4564c79247c9ce8c73175874d549b5ac2428
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2025-09-25 09:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (72.83%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    long long dp[201][201];
-    int solve(vector<vector<int>>&arr,int index,int level){
-          if(index >= arr.size()) return 0;
-          if(dp[index][level] != -1)return dp[index][level];
-          int include=INT_MAX;
-          int exclude=INT_MAX;
+   
+   int minimumTotal(vector<vector<int> > &triangle) {
+    int n = triangle.size();
+    vector<int> minlen(triangle.back());
+    for (int layer = n-2; layer >= 0; layer--) 
+    {
+        for (int i = 0; i <= layer; i++) 
+        {
           
-           
-         include= arr[index][level]+solve(arr,index+1,level);     
-         if(level < arr[index].size())
-         exclude=arr[index][level]+solve(arr,index+1,level+1);
-         return dp[index][level]=min(include,exclude);
-
+            minlen[i] = min(minlen[i], minlen[i+1]) + triangle[layer][i]; 
+        }
     }
-    int minimumTotal(vector<vector<int>>& arr) {
-        memset(dp,-1,sizeof(dp));
-        return solve(arr,0,0);
-    }
+    return minlen[0];
+}
 };
