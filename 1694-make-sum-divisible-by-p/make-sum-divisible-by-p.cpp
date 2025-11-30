dbca76f83c6a6d65cbd64a21d2484e09559cf511
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2025-11-30 12:23:17 +0530

    Time: 171 ms (18.28%) | Memory: 99.3 MB (5.07%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -1,26 +1,33 @@
 class Solution {
 public:
-    int minSubarray(vector<int>& nums, int p) {
-        int ans=INT_MAX,total=0;
-        for(int i=0;i<nums.size();i++){
-                 total=(total+nums[i])%p;
+    int minSubarray(vector<int>& nums, int k) {
+    //    (sum - (prefix[i] - prefix[j]) ) % k = 0;
+    //     sum - prefix[i]%k + prefix[j]%k = 0;
+    //     sum%k + prefix[j]%k = prefix[i]%k
+        
+    //     i - j minimize 
+
+    //     j (max)
+    //     i ()
+       long long ans = INT_MAX;
+        map<long long,int> mp;
+        long long sum = accumulate(nums.begin(),nums.end(),0LL);
+        if(sum % k == 0)return 0;
+        vector<long long> prefix;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(i == 0)prefix.push_back(nums[i]);
+            else prefix.push_back(prefix.back()+nums[i]);
         }
-        if(total==0)return 0;
-        int prefix=0;
-        map<int,int> mp;
-        mp[0]=-1;
-        for(int i=0;i<nums.size();i++){
-                 prefix=(prefix+nums[i])%p;
-
-                 int need=(prefix-total+p)%p;
-
-                 if(mp.find(need)!=mp.end()){
-                       ans=min(ans,i-mp[need]);
-                 }
-                 mp[prefix]=i;
+        mp[0]= -1;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(mp.find((prefix[i] % k- sum %k + k)%k) != mp.end())
+            {
+                ans = min(ans,1ll*(i - mp[(prefix[i]%k - sum%k + k)%k]));
+            }
+            mp[prefix[i] % k] = i;
         }
-        if(ans>=nums.size())return -1;
-        return ans;
-
+        return ans >= nums.size() ? -1 : ans;
     }
 };
