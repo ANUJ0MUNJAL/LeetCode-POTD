6a3c8fbbc1e34c0ebb752f2d1b8dc1e6b08d7c3e
Anuj Munjal <104863126+ANUJ0MUNJAL@users.noreply.github.com>
2026-06-10 20:08:24 +0530

    Time: 19 ms (57.57%) | Memory: 84.9 MB (84.53%) - LeetSync

--- 1814-jump-game-vi/jump-game-vi.cpp
+++ 1814-jump-game-vi/jump-game-vi.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
-        
-        priority_queue<pair<int,int>> pq;
-
-        pq.push({nums[0],0});
-
-        int maxi = nums[0];
+        deque<int> dq;
+        dq.push_back(0);
+        vector<int> dp(nums.size(),0);
+        dp[0] = nums[0];
 
         for(int i=1;i<nums.size();i++)
         {
-            while(!pq.empty() and i - pq.top().second > k)pq.pop();
+            while(dq.size() > 0 and  i - dq.front() > k)dq.pop_front();
 
-            auto el = pq.top();
+        
+            dp[i] = dp[dq.front()] + nums[i];
 
-            int value = nums[i] + pq.top().first;
+            while(dq.size() > 0 and dp[dq.back()]  <= dp[i])dq.pop_back();
 
-            maxi = value;
-
-            pq.push({value,i});
+            dq.push_back(i);
         }
-        return maxi;
-
+        return dp[nums.size()-1];
     }
 };
